fe77815797d71af831661a96f9fced5a3d535e1c
UijinJung <tkrk1226@ajou.ac.kr>
2022-01-11 17:09:55 +0900

    test commit

--- src/main/java/com/poscoict/helloworld01/hello/HelloWorld.java
+++ src/main/java/com/poscoict/helloworld01/hello/HelloWorld.java
@@ -3,6 +3,6 @@ package com.poscoict.helloworld01.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!!!!!!!!!!!!!");
 	}
 }
